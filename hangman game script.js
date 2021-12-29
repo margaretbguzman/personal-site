ba1a80d5dbf50a03b7311c6656e02dc101c3e5b0
margaretbguzman <margaretbguzman@gmail.com>
2021-12-29 13:05:07 -0800

    Added two functions but they do not run

--- hangman game script.js
+++ hangman game script.js
@@ -1,9 +1,24 @@
 window.onload = initialize();
 
 function initialize() {
-	var guess = document.getElementById("guess");
+	var guessBox = document.getElementById("guess");
 	var startButton = document.getElementById("start");
-    var possibleWords = ["improve", "baton", "arctic", "strawberry", "shell", "paper", "sandstone", "feline", "kindness", "trapdoor", "taste", "under", "table", "chair", "treat", "rodent", "water", "nasty", "sensible", "train", "hungry", "beard", "yonder", "difficult", "wheat", "apple", "realize", "happen", "mortar", "title", "scratch", "ghastly", "splendid", "garish", "reading", "trash", "terrific", "honey", "syrup", "stand", "striped", "solid", "realty", "wishbone", "wisdom", "elephant", "aardvark", "canine", "absolute", "entertain", "cavern", "dollar", "century"]
-    var word
+    var wordContainer = document.getElementById("wordContainer");
+    var possibleWords = ["improve", "baton", "arctic", "strawberry", "shell", "paper", "glossy", "sandstone", "feline", "kindness", "trapdoor", "taste", "under", "table", "chair", "treat", "rodent", "water", "nasty", "sensible", "train", "hungry", "beard", "yonder", "difficult", "wheat", "apple", "realize", "happen", "mortar", "title", "scratch", "ghastly", "splendid", "garish", "reading", "trash", "terrific", "honey", "syrup", "stand", "striped", "solid", "realty", "wishbone", "wisdom", "elephant", "aardvark", "canine", "absolute", "entertain", "cavern", "dollar", "century"]
+    var word;
+    var wordIndex;
+    var wrongGuesses;
+    var man = document.getElementById("man");
+    document.write("initialized!");
+}
 
+function start() {
+    var wordIndex = Math.floor(Math.random() * possibleWords.length) + 1;
+    var word = possibleWords[wordIndex];
+    wordContainer.innerHTML = word;
+}
+
+function guess() {
+    guess = guessBox.value;
+    wordContainer.innerHTML = guess;
 }
