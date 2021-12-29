01e90bb0d13842d3b21923f8932e24f12155c065
margaretbguzman <margaretbguzman@gmail.com>
2021-12-29 13:40:29 -0800

    shortened words list for debugging

--- hangman game script.js
+++ hangman game script.js
@@ -4,7 +4,7 @@ function initialize() {
 	var guessBox = document.getElementById("guess");
 	var startButton = document.getElementById("start");
     var wordContainer = document.getElementById("wordContainer");
-    var possibleWords = ["improve", "baton", "arctic", "strawberry", "shell", "paper", "glossy", "sandstone", "feline", "kindness", "trapdoor", "taste", "under", "table", "chair", "treat", "rodent", "water", "nasty", "sensible", "train", "hungry", "beard", "yonder", "difficult", "wheat", "apple", "realize", "happen", "mortar", "title", "scratch", "ghastly", "splendid", "garish", "reading", "trash", "terrific", "honey", "syrup", "stand", "striped", "solid", "realty", "wishbone", "wisdom", "elephant", "aardvark", "canine", "absolute", "entertain", "cavern", "dollar", "century"]
+    var possibleWords = ["improve", "baton", "arctic"]
     var word;
     var wordIndex;
     var wrongGuesses;
