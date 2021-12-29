29348ceabf4b2308ed6b3d38be782f5f7b3f914b
margaretbguzman <margaretbguzman@gmail.com>
2021-12-29 12:42:49 -0800

    Added list of possible words

---
+++ hangman game script.js
@@ -0,0 +1,9 @@
+window.onload = initialize();
+
+function initialize() {
+	var guess = document.getElementById("guess");
+	var startButton = document.getElementById("start");
+    var possibleWords = ["improve", "baton", "arctic", "strawberry", "shell", "paper", "sandstone", "feline", "kindness", "trapdoor", "taste", "under", "table", "chair", "treat", "rodent", "water", "nasty", "sensible", "train", "hungry", "beard", "yonder", "difficult", "wheat", "apple", "realize", "happen", "mortar", "title", "scratch", "ghastly", "splendid", "garish", "reading", "trash", "terrific", "honey", "syrup", "stand", "striped", "solid", "realty", "wishbone", "wisdom", "elephant", "aardvark", "canine", "absolute", "entertain", "cavern", "dollar", "century"]
+    var word
+
+}
