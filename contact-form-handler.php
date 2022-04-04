aa85d6cfcf2240d343e7d468ba0c33f0705173a3
margaretbguzman <margaretbguzman@gmail.com>
2022-04-04 08:30:52 -0700

    Removed contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,4 +4,6 @@ $senderAddress = $_POST["emailAddress"];
 $message = $_POST["message"];
 echo "<p>Thank you for sending me a message, ".$name."! I will be in touch shortly.</p>
 <p>Your message:</p>".$message;
+$subj = "Message from ".$name;
+mail("margaretbguzman@gmail.com", $subj, $message);
 ?>
