debb4d1fe7c2d18106bd6ab3e8b4c78a42e1c1f0
margaretbguzman <margaretbguzman@gmail.com>
2023-03-09 13:49:59 -0800

    new branch file for match 3, new student portal

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -5,5 +5,5 @@ $message = $_POST["message"];
 echo "<p>Thank you for sending me a message, ".$name."! I will be in touch shortly.</p>
 <p>Your message:</p>".$message;
 $subj = "Message from ".$name;
-mail("margaretbguzman@gmail.com", $subj, $message);
+mail("margaretbguzman@gmail.com", $subj, $message, "From: root@margaretbguzman.com");
 ?>
